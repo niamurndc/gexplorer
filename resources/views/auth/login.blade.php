e4be9668a121014e2512f5738b7d1421f4551638
Niamur Rahman <52600388+niamurndc@users.noreply.github.com>
2024-09-14 20:50:02 +0600

    login page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,11 @@
 @section('contnet')
 <div class="container vh-100">
     <div class="row d-flex justify-content-center h-100 align-items-center">
+        
         <div class="col-md-4">
             <div class="card bg-dark border border-secondary p-3 text-center">
-                <h3>Login</h3>
+                <h2 class="text-center mb-4">GEXplorer</h2>
+                <h5>Login</h5>
 
                 <a href="{{route('github.login')}}" class="btn btn-light fw-bold my-4"><i class="fa fa-github"></i> Login with GitHub</a>
             </div>
